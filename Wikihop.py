3220be72a8b52352bfc9a90eb7b92699b40d5d8a
Jennifer Grannen <jenngrannen@berkeley.edu>
2018-09-04 12:17:59 -0700

    list of full url links

--- Wikihop.py
+++ Wikihop.py
@@ -8,16 +8,21 @@ def readLinks(url):
     soup = BeautifulSoup(c, "html.parser", parse_only=bodySection)
     #bodySection = soup.select("body > div:nth-of-type(3) > div:nth-of-type(3) > div:nth-of-type(4) > div")
     childrenList = []
-
     for a in soup.find_all('a', href=True):
         childrenList.append(a['href'])
-
-    childrenListFinal = [l for l in childrenList if "/wiki/" in l]
-    childrenListFinal = [l for l in childrenListFinal if "action=edit" not in l]
-    return childrenListFinal
+    return childrenList
 
 def convertLinkList(list):
     result = [l for l in list if (".png" and ".svg" and ".jpg" and ":") not in l ]
+    result = [l for l in result if "/wiki/" in l]
+    result = [l for l in result if "action=edit" not in l]
     return result
 
-print(convertLinkList(readLinks("https://en.wikipedia.org/wiki/Manu_propria")))
+def rewriteLinkList(list):
+    result = []
+    for l in list:
+        result.append("https://en.wikipedia.org" + l)
+    return result
+
+filteredList = convertLinkList(readLinks("https://en.wikipedia.org/wiki/Manu_propria"))
+print(rewriteLinkList(filteredList))
