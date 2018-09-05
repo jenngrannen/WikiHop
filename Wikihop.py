3f2a63490462844bd1ba6b9352388e32a30263a4
Jennifer Grannen <jenngrannen@berkeley.edu>
2018-09-04 17:47:06 -0700

    it works

--- Wikihop.py
+++ Wikihop.py
@@ -12,7 +12,7 @@ def readLinks(url):
         childrenList.append(a['href'])
     return childrenList
 
-def convertLinkList(list):
+def filterLinkList(list):
     result = [l for l in list if (".png" and ".svg" and ".jpg" and ":") not in l ]
     result = [l for l in result if "/wiki/" in l]
     result = [l for l in result if "action=edit" not in l]
@@ -24,5 +24,63 @@ def rewriteLinkList(list):
         result.append("https://en.wikipedia.org" + l)
     return result
 
-filteredList = convertLinkList(readLinks("https://en.wikipedia.org/wiki/Manu_propria"))
-print(rewriteLinkList(filteredList))
+class LinkClass:
+    linkName = ""
+    prevLink = None
+
+    def __init__(self, linkName, prevLink):
+        self.linkName = linkName
+        self.prevLink = prevLink
+
+def convertListObjects(list, parent):
+    result = []
+    for l in list:
+        result.append(LinkClass(l, parent))
+    return result
+
+def printObjList(list):
+    if list == None:
+        return
+    for l in list:
+        print(l.linkName)
+    #    print(l.prevLink.linkName)
+
+def check(list, endURL):
+    for l in list:
+        if l.linkName == endURL:
+            return True
+    return False
+
+def getFinalLinks(start):
+    url = start.linkName
+    list = readLinks(url)
+    list = filterLinkList(list)
+    list = rewriteLinkList(list)
+    list = convertListObjects(list, start)
+    return list
+
+def runIt(startURL, endURL):
+    list = [LinkClass(startURL, None)]
+    count = 0
+    while not check(list, endURL) and count < 5:
+        tempList = []
+        for l in list:
+            tempList.extend(getFinalLinks(l))
+            #printObjList(tempList)
+        list = tempList
+        count = count + 1
+    if check(list, endURL):
+        goal = [l for l in list if l.linkName == endURL]
+        path = [goal[0].linkName]
+        p = goal[0]
+        while p.prevLink != None:
+            path.append(p.prevLink.linkName)
+            p = p.prevLink
+        return path[::-1]
+    return None
+
+
+
+starturl = "https://en.wikipedia.org/wiki/Manu_propria"
+endurl = "https://en.wikipedia.org/wiki/Roman_Republic"
+print(runIt(starturl, endurl))
